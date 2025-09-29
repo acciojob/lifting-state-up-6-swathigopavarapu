bb2dbd5b3c33a71c52f7ad507a2710bc6d007470
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-29 16:10:54 +0530

    initial commit: todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,17 +3,27 @@ import TodoItem from "./TodoItem";
 
 function TodoList({ todos, onComplete }) {
   return (
-    <ul>
-      {todos.map((todo, index) => (
-        <TodoItem
-          key={index}
-          todo={todo}
-          index={index}
-          onComplete={() => onComplete(index)}
-        />
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ marginBottom: "10px" }}>
+          <span
+            style={{
+              marginRight: "10px",
+              textDecoration: todo.completed ? "line-through" : "none",
+            }}
+          >
+            {todo.text}
+          </span>
+
+          {/* Only show button if todo is NOT completed */}
+          {!todo.completed && (
+            <button onClick={() => onComplete(todo.id)}>Complete</button>
+          )}
+        </li>
       ))}
     </ul>
   );
 }
+
 
 export default TodoList;
